41d5b90d80302fd1b3f83a9ae7559189d0f13859
taotao <oncwnuKJY6G8EpTfb-s3tMZD7Ers@git.weixin.qq.com>
2020-02-21 23:19:00 +0800

    webpack.dev.js merge webpack.prod.js

--- build/webpack.common.js
+++ build/webpack.common.js
@@ -1,24 +1,5 @@
-const path = require('path');
-
-const HtmlWebpackPlugin = require('html-webpack-plugin'); //自动生成html文件
-
-//A webpack plugin to remove your build folder(s) before building 
-// https://github.com/johnagan/clean-webpack-plugin 
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-
-
-const webpack = require('webpack');
-
-//sourceMap: 映射关系 最佳实践
-//{mode: 'production',devtool: 'cheap-module-source-map'}
-//{mode: 'development',devtool: 'cheap-module-eval-source-map'}
-
-
 module.exports = {
-	mode:'production',
-	// devtool: 'none',//关闭开发模式 sourceMap
-	devtool:  'cheap-module-source-map',
- 	entry: {
+	entry: {
  		main: './src/index.js',
  		// 多入口文件
  		sub: './src/sub.js',
@@ -111,15 +92,5 @@ module.exports = {
    		//publicPath: 'http://cdn-webpacker.com', //生成url
    		// publicPath: '/assets/' //生成一个新文件夹
 
-   	},
-   	plugins: [
-   	 	new CleanWebpackPlugin(), //清空上次打包的文件
-	   	new HtmlWebpackPlugin({
-	   		template: 'src/index.html', //指定生成html文件饿模版
-	   		title: 'plugins'
-	   	}),
-	   	//热模块更新
-	   	// new webpack.HotModuleReplacementPlugin(),
-   	 
-   	]
+   	}
 }
--- build/webpack.prod.js
+++ build/webpack.prod.js
@@ -13,27 +13,13 @@ const webpack = require('webpack');
 //{mode: 'production',devtool: 'cheap-module-source-map'}
 //{mode: 'development',devtool: 'cheap-module-eval-source-map'}
 
-
 const merge = require('webpack-merge');
 const commonConfig = require('./webpack.common.js');
 
-const devConfig = {
-	mode:'development',
+const prodConfig = {
+	mode:'production',
 	// devtool: 'none',//关闭开发模式 sourceMap
-	devtool:  'cheap-module-eval-source-map',
- 	//webpack-dev-server
- 	devServer:{
- 		contentBase: './dist',
- 		open: false, //自动打开游览器
- 		port: 8080, 		
- 		proxy: {
- 			'/api': "http: //localhost: 3000"
- 		},
- 		
- 		//热模块更新
- 		hot: true,
- 		hotOnly: true
- 	},
+	devtool:  'cheap-module-source-map',
    	plugins: [
    	 	new CleanWebpackPlugin(), //清空上次打包的文件
 	   	new HtmlWebpackPlugin({
@@ -41,14 +27,8 @@ const devConfig = {
 	   		title: 'plugins'
 	   	}),
 	   	//热模块更新
-	   	new webpack.HotModuleReplacementPlugin(),
+	   	// new webpack.HotModuleReplacementPlugin(),
    	 
-   	],
-
-   	// Tree Shaking
-   	optimization:{
-   		usedExports: true 
-   	}
+   	]
 }
-
-module.exports = merge(commonConfig,devConfig)
+module.exports = merge(commonConfig,prodConfig);
